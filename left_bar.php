e405c1bcf3018f02661e74a7b5e5cc9e8d2b4f7f
Rakesh Mondal <mondalrakesh461@gmail.com>
2018-07-23 14:51:35 +0530

    first commit

---
+++ left_bar.php
@@ -0,0 +1,60 @@
+<div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search...">
+                                <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+                            <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-download" aria-hidden="true"></i> Download <span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="down.php">Download Software</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+						
+						<li>
+                            <a href="#"><i class="fa fa-database"></i> Query<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+							
+								<li>
+                                    <a href="selectquery.php"><i class="fa fa-reorder"></i> Select</a>
+                                </li>
+								<li>
+                                    <a href="receivedata.php"> <i class="fa fa-stack-overflow"></i> Receive Data</a>
+                                </li>
+                                <li>
+                                    <a href="insertquery.php"><i class="fa fa-plus-circle"></i> Insert</a>
+                                </li>
+                                <li>
+                                    <a href="updatequery.php"><i class="fa fa-refresh fa-spin" style="font-size:16px"></i> Update</a>
+                                </li>
+                                 <li>
+                                    <a href="allsearch.php"><i class="fa fa-search"></i> All Search</a>
+                                </li>
+                                 <li>
+                                    <a href="editlog.php"><i class="fa fa-file-code-o"></i> Edit Log</a>
+                                </li>
+								 <li>
+                                    <a href="blankcheck.php"><i class="fa fa-file-code-o"></i> Blank Check</a>
+                                </li>
+                            </ul>
+                           
+                        </li>
+
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
---
+++ selectquery.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Bootstrap Admin Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            
+            <!-- /.navbar-top-links -->
+			<?php
+			include('header.php');
+			?>
+            <?php
+			include('left_bar.php');
+			?>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Fetch Data Query</h1>
+                </div>
+ 
+            </div>
+			
+			<form name="f1" method="post" action="selectquerys.php">
+			<div class="row">
+			<div class="col-lg-2">
+			<span style="font-size:24px;">Table Name:</span>
+			</div>
+			<div class="col-lg-4">
+			<input type="text" name="tblnm" id="tblnm" class="form-control">
+			</div>
+		
+			</div><br>
+			<div class="row">
+			<div class="col-lg-8">
+			<textarea name="qr" id="qr" class="form-control" rows="8"></textarea>
+			</div>
+			</div><br>
+			<div class="row">
+			<div class="col-lg-8">
+			<input type="submit" name="" value="Make Query" class="btn btn-success">
+			</div>
+			</div>
+			</form>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+
+
+    <!-- Custom Theme JavaScript -->
+    <script src="dist/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
